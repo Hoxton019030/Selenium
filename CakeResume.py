8504fbfa25cc3bde2c191e7216269c4093068be5
Hoxton <za546321@yahoo.com.tw>
2023-08-05 14:26:15 +0800

    add gitignore

--- CakeResume.py
+++ CakeResume.py
@@ -7,23 +7,28 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 workbook = openpyxl.Workbook()
 sheet = workbook.active
-headers = ["職務名稱", "公司名稱", "相關描述", "工作簡述", "公司地址", "薪資", "網址連結"]
+headers = ["職務名稱", "公司名稱", "相關描述", "公司地址", "薪資", "網址連結"]
 sheet.append(headers)
-for i in range(0, 36):
+sheet.freeze_panes = 'A2'
+
+for i in range(0, 100):
+
     print("第" + str(i + 1) + "頁")
     url = 'https://www.cakeresume.com/jobs/JAVA?location_list%5B0%5D=%E5%8F%B0%E5%8C%97%E5%B8%82%2C%20%E5%8F%B0%E7%81%A3&location_list%5B1%5D=%E5%8F%B0%E7%81%A3%E5%8F%B0%E5%8C%97%E5%B8%82&location_list%5B2%5D=%E6%96%B0%E5%8C%97%E5%B8%82%2C%20%E5%8F%B0%E7%81%A&page=' + str(
         i + 1)
     driver.get(url)
-    time.sleep(2)
+    driver.implicitly_wait(20)
 
     jobTitle = driver.find_elements(By.CLASS_NAME, 'JobSearchItem_jobTitle__Fjzv2')
-    link = driver.find_elements(By.CLASS_NAME, 'JobSearchItem_jobTitle__Fjzv2')
+    if not jobTitle:
+        break
     companyName = driver.find_elements(By.CLASS_NAME, 'JobSearchItem_companyName__QKkj5')
     describe = driver.find_elements(By.CLASS_NAME, 'JobSearchItem_description__tNSbN')
     address = driver.find_elements(By.CSS_SELECTOR,
                                    '.JobSearchItem_featureSegments__I1Csc > span ')
     salary = driver.find_elements(By.CSS_SELECTOR,
                                   '.InlineMessage_inlineMessage__I9C_W.InlineMessage_inlineMessageLarge__yeH0A.InlineMessage_inlineMessageDark__rNo_a')
+    link = driver.find_elements(By.CLASS_NAME, 'JobSearchItem_jobTitle__Fjzv2')
 
     # for e in link:
     #     print(e.get_attribute('href'))
@@ -34,7 +39,6 @@ for i in range(0, 36):
 
     for j in range(len(jobTitle)):
         try:
-            print(jobTitle[j].text)
             test = [jobTitle[j].text,
                     companyName[j].text,
                     describe[j].text,
@@ -44,6 +48,7 @@ for i in range(0, 36):
                     ]
             sheet.append(test)
         except Exception as e:
+            print(jobTitle[j].text)
             print("An error occurred:", e)
             continue
 
